fbae0a0719909aa0f3f292ffa02afa98989ca921
Anton Skripin <Anton Skripin@DESKTOP-KHJV2AJ.local>
2023-01-16 23:57:10 +0100

    Related work is ready, started implementation part

---
+++ end-user-modeling-constraint-framework/src/main/java/anton/skripin/development/eumcf/EndUserModelingConstraintFrameworkApplication.java
@@ -0,0 +1,13 @@
+package anton.skripin.development.eumcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndUserModelingConstraintFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndUserModelingConstraintFrameworkApplication.class, args);
+	}
+
+}
