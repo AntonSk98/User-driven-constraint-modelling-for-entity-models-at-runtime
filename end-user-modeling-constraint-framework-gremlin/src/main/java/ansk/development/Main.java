15571e3f5de52d22475f114dfc80719e6110ea62
Anton Skripin <Anton Skripin@DESKTOP-KHJV2AJ.local>
2023-02-15 08:23:43 +0100

    Refactor

--- end-user-modeling-constraint-framework-gremlin/src/main/java/ansk/development/Main.java
+++ end-user-modeling-constraint-framework-gremlin/src/main/java/ansk/development/Main.java
@@ -1,16 +1,7 @@
 package ansk.development;
 
-import ansk.development.mapper.GremlinConstraintMapper;
-import anton.skripin.development.domain.instance.InstanceElement;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
